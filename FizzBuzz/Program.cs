cb0e07947ae12567a1bc0ff2468993c7475d0a1b
J3asy <jundachen10@gmail.com>
2023-10-22 14:47:33 -0400

    ✨ modulus 3 and 5

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,10 +5,23 @@ namespace FizzBuzz
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
            for (int i = 0; i < 101; i++) // for loop 0 to 100 increment +1
             {
-                Console.WriteLine($"{i}");
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
